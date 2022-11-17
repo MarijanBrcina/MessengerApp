b3b301953964d0b467400e6004fa1bd85df66ef2
Marijan <marijan1178@gmail.com>
2022-11-17 16:41:34 +0100

    Prijava u aplikaciju koristeći podatke iz Firebase-a

--- app/src/androidTest/java/hr/unipu/android/messengerapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/hr/unipu/android/messengerapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("hr.unipu.android.messangerapp", appContext.getPackageName());
+        assertEquals("hr.unipu.android.messengerapp", appContext.getPackageName());
     }
 }
--- app/src/main/java/hr/unipu/android/messengerapp/activities/LoginActivity.java
+++ app/src/main/java/hr/unipu/android/messengerapp/activities/LoginActivity.java
@@ -2,18 +2,35 @@ package hr.unipu.android.messengerapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Document;
+
 import hr.unipu.android.messengerapp.databinding.ActivityLoginBinding;
+import hr.unipu.android.messengerapp.utilities.Constants;
+import hr.unipu.android.messengerapp.utilities.PreferenceManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        if (preferenceManager.getBoolean(Constants.IS_SIGNED_IN)){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
@@ -22,5 +39,61 @@ public class LoginActivity extends AppCompatActivity {
     private void setListeners (){
         binding.textRegistration.setOnClickListener(view ->
                 startActivity((new Intent(getApplicationContext(), SignUpActivity.class))));
+        binding.buttonLogin.setOnClickListener(v -> {
+            if (validation()) {
+                login();
+            }
+        });
+    }
+
+    private void login(){
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.USERS)
+                .whereEqualTo(Constants.EMAIL, binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.PASSWORD, binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() >0) {
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.IS_SIGNED_IN, true);
+                        preferenceManager.putString(Constants.USER_ID, documentSnapshot.getId());
+                        preferenceManager.putString(Constants.NAME, documentSnapshot.getString(Constants.NAME));
+                        preferenceManager.putString(Constants.IMAGE, documentSnapshot.getString(Constants.IMAGE));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    } else {
+                        loading(false);
+                        showToast("Nije moguće izvršiti prijavu, provjerite točnost podataka");
+                    }
+                });
+    }
+    private void loading(Boolean Loading) {
+        if (Loading){
+            binding.buttonLogin.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonLogin.setVisibility(View.VISIBLE);
+        }
+    }
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean validation(){
+        if (binding.inputEmail.getText().toString().trim().isEmpty()) {
+            showToast("Unesite email adresu");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()) {
+            showToast("Email adresa nije ispravna");
+            return false;
+        }else if (binding.inputPassword.getText().toString().trim().isEmpty()) {
+            showToast("Unesite lozinku");
+            return false;
+        }else {
+            return true;
+        }
     }
 }
